e8316432e98486040eb68745b52bd4fb379d9aac
aalikorn <139930888+aalikorn@users.noreply.github.com>
2024-03-31 13:04:34 +0300

    Update handle_answer.py

--- game/handle_answer.py
+++ game/handle_answer.py
@@ -4,7 +4,6 @@ from states import Menu, QuizMenu
 from aiogram.fsm.context import FSMContext
 from game.send_question import send_question
 from game.finish_quiz import finish_quiz
-from game.start_quiz import current_game
 
 
 router = Router()
@@ -12,6 +11,8 @@ router = Router()
 
 @router.message(QuizMenu.game)
 async def handle_answer(message: Message, state: FSMContext):
+    data_dict = await state.get_data()
+    current_game = data_dict["person"]
     if message.text.lower() == 'завершить квиз':
         current_game.current_question = len(current_game.questions)
     else:
@@ -23,7 +24,15 @@ async def handle_answer(message: Message, state: FSMContext):
         await state.set_state(QuizMenu.finish)
         await finish_quiz(message, current_game, state)
     else:
-        await send_question(message, current_game)
+        await send_question(message)
+
+
+
+
+
+
+
+
 
 
 
