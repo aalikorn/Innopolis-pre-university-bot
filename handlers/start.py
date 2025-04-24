0a8181d7736ec2e992949f67844161699b5e7285
Daria Nikolaeva <nikolaevdaaa@gmail.com>
2025-04-25 02:45:26 +0300

    убрала регу

--- handlers/start.py
+++ handlers/start.py
@@ -13,15 +13,15 @@ router = Router()
 
 @router.message(CommandStart())
 async def start(message: Message, state: FSMContext):
-    await database.db_start()
-    if not await database.is_exists(message.from_user.id):
-        await state.set_state(Form.wait)
-        await database.db_start()
-        await message.answer(f"Перед тем, как начать, вам нужно заполнить небольшую анкету. Нажмите /form, чтобы начать")
-    else:
-        await message.answer(
-            text=messages.menuMessage,
-            reply_markup=markup.menu_kb,
-            parse_mode='html')
-        await state.set_state(Menu.menu)
+    # await database.db_start()
+    # if not await database.is_exists(message.from_user.id):
+    #     await state.set_state(Form.wait)
+    #     await database.db_start()
+    #     await message.answer(f"Перед тем, как начать, вам нужно заполнить небольшую анкету. Нажмите /form, чтобы начать")
+    # else:
+    await message.answer(
+        text=messages.menuMessage,
+        reply_markup=markup.menu_kb,
+        parse_mode='html')
+    await state.set_state(Menu.menu)
 
